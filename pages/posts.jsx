9dc8da46b82d6a51637f4c3589c46603f806f741
Jia Le <88878287+JiaLe0709@users.noreply.github.com>
2023-10-04 23:17:23 +0800

    Update

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -2,7 +2,7 @@ import Layout from '@/layouts/Glayouts'
 import { getAllPosts, getPostBlocks } from '@/lib/notion'
 import Article from '@/components/Home/articleBlock'
 
-export async function getStaticProps() {
+export async function getStaticProps({ req, res }) {
     const posts = await getAllPosts({ onlyPost: true })
   
     const heros = await getAllPosts({ onlyHidden: true })
@@ -16,6 +16,11 @@ export async function getStaticProps() {
       return { props: { post: null, blockMap: null } }
     }
   
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=86400, stale-while-revalidate=59'
+    )
+
     return {
       props: {
         blockMap,
