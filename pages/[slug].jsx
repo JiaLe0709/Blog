f726aafaad3c49b5294e3bf6898a5ff1d2832d73
Jia Le <88878287+JiaLe0709@users.noreply.github.com>
2023-10-05 23:20:13 +0800

    Update

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,6 +1,6 @@
 import Layout from '@/layouts/postLayout'
 import { getAllPosts, getPostBlocks } from '@/lib/notion'
-// import BLOG from '@/blog.config'
+import BLOG from '@/blog.config'
 import { useRouter } from 'next/router'
 import { NotFoundError } from 'next/error'
 
