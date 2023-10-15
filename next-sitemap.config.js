ee8f8f74a946ea14726b708d5690fc797321b9df
Jia Le <jle26531@gmail.com>
2023-10-15 13:30:23 +0000

    Seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,8 @@ const BLOG = require('./blog.config')
 module.exports = {
   siteUrl: BLOG.link,
   generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
   generateIndexSitemap: false,
   sitemapSize: 7000
 }
